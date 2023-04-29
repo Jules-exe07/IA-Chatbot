ba65803d6fa186b76a03b11c3a6f955a4331929b
Julian Londoño <Julianflp1@gmail.com>
2023-04-29 17:00:12 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,18 @@
 from flask import Flask
+
+# Create Flask app instance
 def create_app():
     app = Flask(__name__)
+
+    # Set a secret key to enable session management
     app.config['SECRET_KEY'] = 'Here we go again boi'
 
+    # Import and register blueprints for different views and functionalities
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    return app 
+    # Return the created app instance
+    return app
