b6f4ca9244e343c534ec2c8625630eebb3d0ea92
Julian Londoño <Julianflp1@gmail.com>
2023-04-29 16:56:33 -0500

    Update embedding.py

--- embedding.py
+++ embedding.py
@@ -1,27 +1,32 @@
 import openai
-import numpy as np  # standard math module for python
+import numpy as np
 from pprint import pprint
 
 
+# Function to open a file and read its content
 def open_file(filepath):
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
 
 
+# Function to generate GPT-3 embeddings for a given text
 def gpt3_embedding(content, engine='text-similarity-babbage-001'):
-    content = content.encode(encoding='UTF-8',errors='replace').decode()
-    response = openai.Embedding.create(input=content,engine=engine)
+    content = content.encode(encoding='UTF-8', errors='replace').decode()
+    response = openai.Embedding.create(input=content, engine=engine)
     vector = response['data'][0]['embedding']  # this is a normal list
     return vector
 
 
-def similarity(v1, v2):  # return dot product of two vectors
+# Function to calculate the dot product (similarity) between two vectors
+def similarity(v1, v2):
     return np.dot(v1, v2)
 
 
+# Set OpenAI API key by reading it from a file
 openai.api_key = open_file('openaiapikey.txt')
 
 
+# Function to match a given vector to a list of pre-defined classes
 def match_class(vector, classes):
     results = list()
     for c in classes:
@@ -32,17 +37,22 @@ def match_class(vector, classes):
 
 
 if __name__ == '__main__':
-    categories = ['']
+    categories = ['']  # Add categories here
     classes = list()
+    
+    # Generate embeddings for each category and store them in a list
     for c in categories:
         vector = gpt3_embedding(c)
         info = {'category': c, 'vector': vector}
         classes.append(info)
-    #print(classes)
-    #exit(0)
+    
+    # Uncomment these lines to print the list of classes and exit the program
+    # pprint(classes)
+    # exit(0)
+    
+    # Start a loop to continually ask for user input and match it to pre-defined classes
     while True:
         a = input('Enter a lifeform here: ')
         vector = gpt3_embedding(a)
-        #print(a, vector)
         result = match_class(vector, classes)
         pprint(result)
